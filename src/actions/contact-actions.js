93d8dfec22d6c9144559fff872a4633c3473cf41
Caglar Ergul <caglarergul@me.com>
2017-11-23 17:15:29 +0300

    no message

---
+++ src/actions/contact-actions.js
@@ -0,0 +1,10 @@
+import {client} from './';
+const url = '/contacts';
+export function fetchContacts() {
+    return dispatch => {
+        dispatch({
+            type: 'FETCH_CONTACTS',
+            payload: client.get(url)
+        })
+    }
+}
---
+++ src/reducers/contact-reducer.js
@@ -0,0 +1,19 @@
+const defaultState = {
+    contacts: []
+}
+
+export default(state = defaultState, action = {}) => {
+    switch (action.type) {
+        case 'FETCH_CONTACTS_FULFILLED':
+            {
+                return {
+                    ...state,
+                    contacts: action.payload.data.data || action.payload.data
+
+                }
+            }
+       
+        default:
+            return state;
+    }
+}
